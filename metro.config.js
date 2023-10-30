05688df9fd7c544e09c6753af84f0cab9d2c4ce5
Vladislav Ezerski <vladezerski@gmail.com>
2023-10-30 21:16:55 +0100

    added some project settings

--- metro.config.js
+++ metro.config.js
@@ -9,3 +9,32 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+
+// const path = require('path');
+
+// module.exports = {
+//   resolver: {
+//     extraNodeModules: new Proxy(
+//       {},
+//       {
+//         get: (target, name) => path.join(process.cwd(), `./src/${name}`),
+//       }
+//     ),
+//   },
+// };
+
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+//   resolver: {
+//       nodeModulesPaths: [...localPackagePaths], // update to resolver
+//   },
+//   watchFolders: [...localPackagePaths], // update to watch
+// };
